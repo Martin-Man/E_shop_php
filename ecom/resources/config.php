a3d3fdb434a02f092fb37a9999e6b31adbb0f8e9
Martin-Man <60697170+Martin-Man@users.noreply.github.com>
2020-03-22 21:24:15 +0100

    Update config.php

--- ecom/resources/config.php
+++ ecom/resources/config.php
@@ -1,11 +1,16 @@
 <?php
 ob_start();
 session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ecom_db";
 
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
 defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
+
 foreach ($_SERVER as $key => $value) {
     if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
         continue;
@@ -16,14 +21,11 @@ foreach ($_SERVER as $key => $value) {
     $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
     $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ecom_db";
+
 //defined("DB_NAME") ? null : define("DB_NAME",  "ecom_db");
 //defined("DB_NAME") ? null : define("DB_NAME",  "ecom2");
-
-$connection = mysqli_connect($servername, $username, $password, $dbname);
+$connection = new mysqli($servername, $username, $password, $dbname);
+//$connection = mysqli_connect($servername, $username, $password, $dbname);
 
 require_once("functions.php");
 require_once("cart.php");
